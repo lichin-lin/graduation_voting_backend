093ea455b8e9fac6f5dedc8d951c160f85adf920
lichin-lin <vic20087cjimlin.cs03@nctu.edu.tw>
2018-01-04 16:34:57 +0800

    add jwt

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 import csv
 import sqlite3
 from flask import Flask, session, request, redirect, jsonify, g
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 
 # Our oauth
 from oauth import Oauth
@@ -10,8 +12,35 @@ NCTU_APP_REDIRECT_URI = 'http://127.0.0.1:5000/auth'
 NCTU_APP_CLIENT_ID = 'dFo3aTrp02yAzzHgaYNf90IUGe15ASgZfb6Wl2gb'
 NCTU_APP_CLIENT_SECRET = 'dV2NgLReGwmKyfBIGajbVAZCAr7puGyudu1ZianSaIMV441Lo4udlPXloItyQTCGN3aHapPDV4OzNfb91Z1Hfm1HSEQkK9yKLt3vwtUc7JczIeDB7Rfo3nVqVgEuDbTY'
 
+# JWT
+class User(object):
+    def __init__(self, id, usercode):
+        self.id = id
+        self.usercode = usercode
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, '7gTeaXHzIjjRi7UPHqygnBnORpr0hQ'),
+    User(2, '123')
+]
+
+usercode_table = {u.usercode: u for u in users}
+userid_table = {u.id: u for u in users}
+
+def authenticate(usercode):
+    user = usercode_table.get(usercode, None)
+    if user and safe_str_cmp(user.usercode.encode('utf-8'), usercode.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+# JWT
 app = Flask(__name__)
 app.secret_key = 'your super coll secrey key'
+jwt = JWT(app, authenticate, identity)
 SQLITE_DB_PATH = 'songs.db'
 SQLITE_DB_SCHEMA = 'create_db.sql'
 MEMBER_CSV_PATH = 'songs.csv'
@@ -72,11 +101,15 @@ def auth():
     if code:
         #get user token
         if nctu.get_token(code):
-            url = 'http://127.0.0.1:3000/?auth=' + code
+            url = 'http://127.0.0.1:3000/?code=' + code
             return redirect(url)
-            # return jsonify(nctu.get_profile())
 
     return redirect('/login')
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 # SQLite3-related operations
 # See SQLite3 usage pattern from Flask official doc
@@ -98,6 +131,5 @@ def close_connection(exception):
 
 if __name__ == "__main__":
     app.run(debug=1)
-
 
 # FLASK_APP=app.py flask run
